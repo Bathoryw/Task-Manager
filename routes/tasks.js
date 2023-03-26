746ebed539321e4e903ba57e9f13a1e40ee1bb95
emanuel caro <darkemlord@gmail.com>
2023-03-26 20:41:23 +0900

    get y post working

--- routes/tasks.js
+++ routes/tasks.js
@@ -3,11 +3,17 @@ const router = express.Router();
 const Task = require("../models/Task");
 
 /* GET home page. */
-router.get("/", function (_req, res, _next) {
-  res.json({ message: "hello from task manager" });
+router.get("/", async function (_req, res, _next) {
+  try {
+    const allTasks = await Task.find({});
+    res.json(allTasks);
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
 });
 
 router.post("/", async (req, res) => {
+  console.log(req.body);
   try {
     const task = new Task(req.body);
     await task.save();
