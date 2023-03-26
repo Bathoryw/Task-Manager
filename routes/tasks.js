7a402c9085f05d73f10c590d070835eca6ad4e4c
emanuel caro <darkemlord@gmail.com>
2023-03-26 21:09:15 +0900

    server done

--- routes/tasks.js
+++ routes/tasks.js
@@ -13,11 +13,30 @@ router.get("/", async function (_req, res, _next) {
 });
 
 router.post("/", async (req, res) => {
-  console.log(req.body);
   try {
     const task = new Task(req.body);
     await task.save();
     res.status(201).json(task);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+});
+
+router.put("/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const task = await Task.findByIdAndUpdate(id, req.body, { new: true });
+    res.status(201).json(task);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+});
+
+router.delete("/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    await Task.findByIdAndDelete(id);
+    res.status(200).json({ message: "successfully deleted" });
   } catch (err) {
     res.status(400).json({ message: err.message });
   }
