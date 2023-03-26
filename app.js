bdee6c34936f8e3251937ae614fe76432b489f19
emanuel caro <darkemlord@gmail.com>
2023-03-26 20:02:17 +0900

    accion de get y post para tareas creada

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 require("dotenv").config();
 const express = require("express");
-const path = require("path");
-const mongoose = require("mongoose");
-const taskRouter = require("./routes/tasks");
+const cors = require("cors"); // para evitar problemas de permisos
+const mongoose = require("mongoose"); // conecta la base de datos
+const taskRouter = require("./routes/tasks"); // importa las rutas de datos de tareas
 
 const app = express();
 const port = process.env.PORT;
@@ -26,7 +26,11 @@ app.get("/", (req, res) => {
   res.send("Hi babies");
 });
 
+app.use(cors()); // Evita problemas de permisos con la API
 app.use(express.json());
+
+// conecta las rutas de tareas que estan en otro archivo
+// para hacer mas facil el manejo
 app.use("/tasks", taskRouter);
 
 app.listen(port, () => {
